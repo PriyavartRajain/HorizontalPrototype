0e6877a1dc7ec9e2fc6f54d557476ff58fb4ea81
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2021-03-08 02:26:37 -0700

    Added Immigration Services Page 3

---
+++ HorizontalPrototype/ImmServicesWindow_3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HorizontalPrototype
+{
+    /// <summary>
+    /// Interaction logic for ImmServicesWindow_3.xaml
+    /// </summary>
+    public partial class ImmServicesWindow_3 : Window
+    {
+        public ImmServicesWindow_3()
+        {
+            InitializeComponent();
+        }
+    }
+}
