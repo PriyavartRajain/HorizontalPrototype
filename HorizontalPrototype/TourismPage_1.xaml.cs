09a38a60409f5e1580971300ff61d13a32df985a
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2021-03-08 14:29:02 -0700

    Created TourismPage_1

---
+++ HorizontalPrototype/TourismPage_1.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HorizontalPrototype
+{
+    /// <summary>
+    /// Interaction logic for TourismPage_1.xaml
+    /// </summary>
+    public partial class TourismPage_1 : Window
+    {
+        public TourismPage_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
