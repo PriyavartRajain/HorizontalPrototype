3cfaaa7a377bbf6f71efdcc7b7498b5e73c122ab
PriyavartRajain <47432873+PriyavartRajain@users.noreply.github.com>
2021-03-08 21:08:25 -0700

    added login screen 2

---
+++ HorizontalPrototype/LoginScreen_2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HorizontalPrototype
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen_2.xaml
+    /// </summary>
+    public partial class LoginScreen_2 : Window
+    {
+        public LoginScreen_2()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
