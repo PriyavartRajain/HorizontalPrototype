40dd798c5dfc36f505225ead858731193a46df1e
Mohammed Rakeeb <mohammed.rakeeb@ucalgary.ca>
2021-03-26 20:53:07 -0600

    mohammed

---
+++ HorizontalPrototype/Transit2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Transit
+{
+    /// <summary>
+    /// Interaction logic for Transit2.xaml
+    /// </summary>
+    public partial class Transit2 : Window
+    {
+        public Transit2()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ HorizontalPrototype/Transit3.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Transit
+{
+    /// <summary>
+    /// Interaction logic for Transit3.xaml
+    /// </summary>
+    public partial class Transit3 : Window
+    {
+        public Transit3()
+        {
+            InitializeComponent();
+        }
+    }
+}
